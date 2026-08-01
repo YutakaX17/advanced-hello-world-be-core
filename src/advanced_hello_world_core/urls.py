cd9bb57dedee8a5291ec42da45a6dffa5e5a3d97
YutakaX17 <kademptr@gmail.com>
2026-08-01 23:13:43 +0200

    refactor: extract message feature from core

--- src/advanced_hello_world_core/urls.py
+++ src/advanced_hello_world_core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import MessageListCreateView, liveness, readiness
+from .views import liveness, readiness
 
 app_name = "advanced_hello_world_core"
 
 urlpatterns = [
-    path("v1/messages", MessageListCreateView.as_view(), name="messages"),
     path("v1/health/live", liveness, name="liveness"),
     path("v1/health/ready", readiness, name="readiness"),
 ]
