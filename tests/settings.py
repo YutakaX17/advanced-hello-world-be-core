90e1a7e1070ed199e0ee50af4342c9cc6d8724d2
YutakaX17 <kademptr@gmail.com>
2026-08-01 23:24:08 +0200

    fix: remove obsolete model typing plugin

--- tests/settings.py
+++ tests/settings.py
@@ -2,7 +2,6 @@ SECRET_KEY = "test-only-secret"
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "rest_framework",
     "advanced_hello_world_core",
 ]
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
